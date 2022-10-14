edd8c02d503d28c2d333644c6674523f535d59e1
jossparling <jossparling@gmail.com>
2022-10-14 11:51:13 -0500

    added resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,13 +3,13 @@ import DownloadIcon from '@mui/icons-material/Download'
 
 const Resume = () => {
 
-    const skills = ['HTML', 'CSS', 'JavaScript', 'Node.js', 'Express.js', 'React.js', 'MongoDB', 'MySQL']
+    const skills = ['HTML', 'CSS', 'JavaScript', 'Node.js', 'Express.js', 'React.js', 'MongoDB', 'MySQL', 'GraphQL', 'React']
     
     return(
         <section className="resume">
             <div>
                 <h2>Resume</h2>
-                <a href="https://drive.google.com/file/d/10kFUDbmz3-21grN5jEHNfqqFJ-6yuS09/view?usp=sharing"><DownloadIcon fontSize='large' sx={{ color: '#303b47'}} /> Download Resume</a>
+                <a href="https://drive.google.com/file/d/1XAhtSH0AfW12mAzcrHqIS3SwdscQCwSS/view?usp=sharing"><DownloadIcon fontSize='large' sx={{ color: '#303b47'}} /> Download Resume</a>
                 <h3>Skills</h3>
                 <ul className="skills-ul">
                     {skills.map((skill) => (
